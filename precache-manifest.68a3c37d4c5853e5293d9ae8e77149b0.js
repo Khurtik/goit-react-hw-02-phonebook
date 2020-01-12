c8ac9785e856d48161e7ba8866d6bb886be68985
sergiy khurtik <skhurtik@ukr.net>
2020-01-12 19:46:57 +0200

    Updates

---
+++ precache-manifest.68a3c37d4c5853e5293d9ae8e77149b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfd13b9855c8c9cfc1e05886b4216e57",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "c82b8bc0d7fcef75d9d5",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.3f5c05d0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.3f5c05d0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "42327138ae30b56966c8",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.31cf1102.chunk.js"
+  },
+  {
+    "revision": "a71c9662b59c9d18d185",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.633fc205.js"
+  }
+]);
